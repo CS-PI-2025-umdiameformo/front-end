a42ff9cb534260b34895026f9ea35689e0390e53
Pedro <pedrohtr661@gmail.com>
2025-05-13 22:09:26 -0300

    Fix para o script faltante.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from "./components/header/header";
 import Calculadora from "./pages/calculadora/calculadora";
 import Home from "./pages/home/home";
 import Footer from "./components/footer/footer";
+import LoginUsuario from "./pages/loginUsuario/loginUsuario";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Header nome="Tomita" />
       <BrowserRouter>
         <Routes>
+          <Route path="/login" Component={LoginUsuario} />
           <Route path="/" Component={Home} />
           <Route path="/calculadora" Component={Calculadora} />
         </Routes>
--- src/pages/loginUsuario/loginUsuario.jsx
+++ src/pages/loginUsuario/loginUsuario.jsx
@@ -3,7 +3,9 @@ import React from "react";
 const loginUsuario = () => {
   return (
     <>
-
+    <input type="string" />
+    <input type="password" />
+    <button>Login</button>
     </>
   );
 };
