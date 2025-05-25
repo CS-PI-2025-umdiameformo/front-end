94766bdcaa3ec1182d66e1db9f1bf434111ad69b
Pedro Henrique <pedrohtr661@gmail.com>
2025-05-25 09:46:17 -0300

    Style/ Pequenas alterações visuais para manter as pagínas uniformes

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -3,12 +3,5 @@ import "./header.css";
 
 const Header = (params) => {
   const { nome, idade } = params;
-  return (
-    <>
-      <div className="header">
-        <h1>Olá, {nome}</h1>
-      </div>
-    </>
-  );
 };
 export default Header;
