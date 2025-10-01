3055db6ba00db3ae20562d67bfd76a275059dfae
Pedro <Pedrohtr661@gmail.com>
2025-09-30 21:58:36 -0300

    Teste de commit

--- src/.test.js
+++ src/.test.js
@@ -2,7 +2,12 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
-  render(<App />);
+  render(App);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
-});
+},
+  test('testa uma condição que falha', () => {
+
+    render(<App />);
+    expect(true).toBe(false); 
+  }));
